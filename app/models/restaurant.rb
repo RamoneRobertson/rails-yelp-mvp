0c892fc322037670f6045b73f9e1ea503deb6f18
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-02 21:40:16 +0900

    Finished restaurant model validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "Invalid Category"
+  }
 end
