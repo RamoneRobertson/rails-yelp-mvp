81d21a20243e2080c0b41a66e61ebf96f151614a
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-04 23:44:29 +0900

    Updated seeds file to create reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,25 @@ puts "Clearing database..."
 Restaurant.destroy_all
 
 puts "Creating restaurants"
-12.times do
+10.times do
   Restaurant.create!(
     name: Faker::Restaurant.name,
     address: "#{Faker::Address.street_address} #{Faker::Address.zip_code}, #{Faker::Address.city}, #{Faker::Address.state_abbr}, #{Faker::Address.country}",
     phone_number: "#{Faker::PhoneNumber.phone_number}",
     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
   )
+end
+
+puts "Creating reviews..."
+restaurants = Restaurant.all
+restaurants.each do |restaurant|
+  3.times do
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant_id: restaurant.id
+    )
+  end
 end
 
 puts "Done."
