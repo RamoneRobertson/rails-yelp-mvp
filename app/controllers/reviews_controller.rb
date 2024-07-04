22bb3b5f1a40220f75b15a6ac45b47454bec68fa
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-04 23:22:50 +0900

    Finished with add review logic and review form view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+  # User should be able to add a new review for a specific restaurant
+  #----- GET 'restaurants/:id/'
   def new
     @review = Review.new
   end
 
   def create
-    raise
+    @review = Review.new(review_params)
+    @review.restaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def review_params
     params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
